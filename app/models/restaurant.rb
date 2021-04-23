c7949f4f4185fbe7740e956ada04b4b78687b617
Raquel Lima <raqueljones182@gmail.com>
2021-04-22 21:44:10 -0300

    add restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :name, :address, :category, presence: true
+  CATEGORIES = %w(chinese italian japanese french belgian)
+  validates :category, inclusion: { in: CATEGORIES, message: '%{value} is not a valid category' }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+  validates :content, :rating, presence: true
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: (0..5), message: 'Should be between 0 to 5' }
 end
